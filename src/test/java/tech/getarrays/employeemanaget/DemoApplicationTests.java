7ee55d8e302dd281f906ec0f01be3f3a90d489da
[Veronika Paramonova] <[paramovv@gmail.com]>
2021-09-02 16:13:56 -0400

    Initial commit

---
+++ src/test/java/tech/getarrays/employeemanaget/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.getarrays.employeemanaget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
